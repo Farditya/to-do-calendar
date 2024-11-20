42942202e09d693d24ff2b21cce6b702f47900fe
Farditya <adityadaffa972@gmail.com>
2024-11-20 19:33:12 +0700

    instalasi react dan tailwind

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <header className="text-center">
+        <h1 className="text-4xl font-sans font-bold mb-3">React App</h1>
+        <a
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
