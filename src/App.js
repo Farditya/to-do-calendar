43ad97c24f4eefd14a9b8a04dd315a03027d52a5
Farditya <adityadaffa972@gmail.com>
2024-11-26 13:29:57 +0700

    menambahkan fitur tasklist

--- src/App.js
+++ src/App.js
@@ -8,12 +8,10 @@ function App() {
     setTasks([...tasks, task]);
   };
   return (
-    <div className="container mx-auto p-4">
     <div className="flex flex-col justify-center items-center">
       <h1 className="text-2xl font-bold mb-4 text-center">To do list with calendar</h1>
       <TaskForm onAddTask={addTask} />
       <TaskList tasks={tasks} />  
-    </div>
     </div>
   );
 }
